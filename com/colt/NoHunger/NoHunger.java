73e992a57a6846d6abf6ad3b3d8fc2a79a5561f5
Colt <Cewlt@users.noreply.github.com>
2015-01-18 14:31:37 -0500

    Update NoHunger.java

--- com/colt/NoHunger/NoHunger.java
+++ com/colt/NoHunger/NoHunger.java
@@ -3,6 +3,7 @@ package com.colt.NoHunger;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,8 +13,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class NoHunger extends JavaPlugin implements Listener{
        
         public void onEnable(){
-        saveDefaultConfig();
-        Bukkit.getPluginManager().registerEvents(this, this);
+        	saveDefaultConfig();
+            Bukkit.getPluginManager().registerEvents(this, this);
         }
        
         @EventHandler
@@ -23,7 +24,8 @@ public class NoHunger extends JavaPlugin implements Listener{
                 	if(p.hasPermission("nohunger.use")) {
                 	List<String> worlds = getConfig().getStringList("worlds");
                 	for(String w : worlds) {
-                		if(p.getWorld().equals(w)) {
+            			World world = Bukkit.getWorld(w);
+            			if(p.getWorld().equals(world)) {
                 			e.setCancelled(true);
                 			p.setFoodLevel(20);
                 		}
