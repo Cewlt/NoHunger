11010331f257f2343f6e7a5b975968e18f2dc260
Colt <Cewlt@users.noreply.github.com>
2016-05-21 23:25:03 -0400

    Update NoHunger.java

--- com/colt/NoHunger/NoHunger.java
+++ com/colt/NoHunger/NoHunger.java
@@ -1,39 +1,49 @@
-package com.colt.NoHunger;
- 
+package com.colt.nohunger;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class NoHunger extends JavaPlugin implements Listener {
-       
-        public void onEnable() {
-	  saveDefaultConfig();
-          Bukkit.getPluginManager().registerEvents(this, this);
-        }
-       
-    @EventHandler
-    public void onFoodLevelChange(FoodLevelChangeEvent event) {
-        if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            if (player.hasPermission("nohunger.use")) {
-                List<String> worlds = getConfig().getStringList("worlds");
-                for (String w : worlds) {
-                    World world = Bukkit.getWorld(w);
-                    if (player.getWorld().equals(world)) {
-                        if (player.getHealth() != 20.0) {
-                            player.setHealth(20.0D);
-                        }
-                        event.setCancelled( true );
-                        break;
-                    }
-                }
-            }
-        }
-    }
+
+	List<String> worlds = new ArrayList<String>();
+
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+
+		for (String world : getConfig().getStringList("worlds")) {
+			if (Bukkit.getWorld(world) == null) {
+				getLogger().info("Invalid world in config: " + world);
+				continue;
+			} else {
+				worlds.add(world);
+				continue;
+			}
+		}
+	}
+
+	@EventHandler
+	public void foodChangeEvent(FoodLevelChangeEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER) {
+			Player player = (Player) event.getEntity();
+			if (player.hasPermission("no.hunger")) {
+				if (worlds.isEmpty())
+					return;
+				if (worlds.contains(player.getWorld().getName())) {
+					event.setCancelled(true);
+					if (player.getFoodLevel() < 19.0)
+						player.setFoodLevel(20);
+				}
+			}
+		}
+	}
 }
